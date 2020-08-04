03ec28ed2a5d092229d8a474c2a3a9deba369e79
Clauderic Demers <me@ced.io>
2020-08-04 11:58:06 -0400

    feat: Add an onPaste default handler for android

--- packages/slate-react/src/plugins/android/index.js
+++ packages/slate-react/src/plugins/android/index.js
@@ -1,5 +1,14 @@
+import Debug from 'debug'
+import Plain from '@slate-fork/slate-plain-serializer'
 import getWindow from 'get-window'
 import CompositionManager from './composition-manager'
+import getEventTransfer from '../../utils/get-event-transfer'
+
+/**
+ * @type {Debug}
+ */
+
+const debug = Debug('slate:android')
 
 /**
  * Fixes a selection within the DOM when the cursor is in Slate's special
@@ -112,8 +121,46 @@ function AndroidPlugin({ editor }) {
     editor.blur()
   }
 
+  /**
+   * Default paste handler if no other more specific user plugin handles onPaste.
+   *
+   * @param {Event} event
+   * @param {Editor} editor
+   * @param {Function} next
+   */
+
+  function onPaste(event, _editor, next) {
+    debug('onPaste', { event })
+
+    const { value } = editor
+    const transfer = getEventTransfer(event)
+    const { type, fragment, text } = transfer
+
+    if (type === 'fragment') {
+      event.preventDefault()
+      editor.insertFragment(fragment)
+    }
+
+    if (type === 'text' || type === 'html') {
+      if (!text) return next()
+      const { document, selection, startBlock } = value
+      if (editor.isVoid(startBlock)) return next()
+
+      event.preventDefault()
+
+      const defaultBlock = startBlock
+      const defaultMarks = document.getInsertMarksAtRange(selection)
+      const frag = Plain.deserialize(text, { defaultBlock, defaultMarks })
+        .document
+      editor.insertFragment(frag)
+    }
+
+    next()
+  }
+
   return {
     onBlur,
+    onPaste,
     onComponentDidMount,
     onComponentDidUpdate,
     onComponentWillUnmount,
